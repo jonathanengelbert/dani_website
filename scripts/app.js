8b33afc6cb20bd93a6d00e957fa8e9710cfa4632
Jonathan Engelbert <jonathanengelbert@gmail.com>
2018-04-15 17:21:24 -0700

    Mobile version ready, except for contact form

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
+
+
+//=================================================================
+//SCROLLING
+//=================================================================
+
 //Anchor Links:
 const bioLink = document.getElementById('bio-link');
 const musicLink = document.getElementById('music-link');
@@ -27,16 +33,135 @@ musicLink.addEventListener('click', function(){
 });
 
 //LISTENERS
-// homeLink.addEventListener('click', function(){
-//   homeAnchor.scrollIntoView({
-//     behavior: 'smooth'
-//   });
-// });
+homeLink.addEventListener('click', function(){
+  homeAnchor.scrollIntoView({
+    behavior: 'smooth'
+  });
+});
 
 //LISTENERS
 contactLink.addEventListener('click', function(){
-  console.log(this);
   contactAnchor.scrollIntoView({
     behavior: 'smooth'
   });
+});
+
+//=================================================================
+//PLAYER
+//=================================================================
+
+const button = document.querySelector('.button__play');
+const songs = document.querySelector('#songs');
+
+//songs
+const islands = document.getElementById('islands').src;
+const noWords = document.getElementById('no_words').src;
+const ritual = document.getElementById('ritual').src;
+
+//song triggers
+const islandsTrigger = document.querySelectorAll('.song')[0];
+const noWordsTrigger = document.querySelectorAll('.song')[1];
+const ritualTrigger = document.querySelectorAll('.song')[2];
+
+
+//player
+const progress = document.querySelectorAll('.progress');
+
+let intervalId;
+
+
+
+//play button
+button.addEventListener("click", function(){
+if(songs.src===""){
+  songs.src=islands;
+}
+togglePlayPause();
+if(songs.src==islands){
+  intervalId = setInterval(function(){getTrackProgress(0)}, 1000);
+}
+if(songs.src==noWords){
+  intervalId = setInterval(function(){getTrackProgress(1)}, 1000);
+}
+if(songs.src==ritual){
+  intervalId = setInterval(function(){getTrackProgress(2)}, 1000);
+}
+});
+
+function togglePlayPause(){
+  clearInterval(intervalId);
+  if(songs.paused === true) {
+    button.className = "button__pause";
+    return songs.play();
+
+  }
+  if(songs.paused === false) {
+  button.className = "button__play";
+  return songs.pause();
+
+  }
+
+}
+
+function resetPlayer(){
+  if(songs.currentTime > 0) songs.currentTime = 0;
+}
+
+//song listeners
+islandsTrigger.addEventListener("click", function(){
+    if(songs.src!=islands) songs.src=islands;
+    togglePlayPause();
+    intervalId = setInterval(function(){getTrackProgress(0)}, 1000);
+
+  });
+
+noWordsTrigger.addEventListener("click", function(){
+    if(songs.src!=noWords) songs.src=noWords;
+    togglePlayPause();
+    intervalId = setInterval(function(){getTrackProgress(1)}, 1000);
+});
+
+ritualTrigger.addEventListener("click", function(){
+  if(songs.src!=ritual) songs.src=ritual;
+  togglePlayPause();
+    intervalId = setInterval(function(){getTrackProgress(2)}, 1000);
+});
+
+
+function getTrackProgress(track){
+  let value = 0;
+  // songs.addEventListener('timeupdate', getTrackProgress, false);
+  if(songs.currentTime > 0) {
+    value = Math.floor((100 / songs.duration) * songs.currentTime);
+  }
+  progress[track].style.width = value + "%";
+
+
+}
+
+//EVENT LISTENER FOR SEEK IN PROGRESS BAR
+
+const progressBar0 = document.querySelectorAll('.progress__bar')[0];
+progressBar0.addEventListener("click", function(event){
+  let position = ((event.clientX - this.offsetLeft) / this.offsetWidth * 100);
+  progress[0].style.width = position+"%";
+  songs.currentTime = position * 2;
+  
+
+});
+const progressBar1 = document.querySelectorAll('.progress__bar')[1];
+progressBar1.addEventListener("click", function(event){
+  let position = ((event.clientX - this.offsetLeft) / this.offsetWidth * 100);
+  progress[1].style.width = position+"%";
+  songs.currentTime = position * 2;
+
+
+});
+const progressBar2 = document.querySelectorAll('.progress__bar')[2];
+progressBar2.addEventListener("click", function(event){
+  let position = ((event.clientX - this.offsetLeft) / this.offsetWidth * 100);
+  progress[2].style.width = position+"%";
+  songs.currentTime = position * 2;
+
+
 });
